4f99bca98e7deaac35c8aa72fdf8ec0d99d1c928
i5dr0id <i5dr0id308@gmail.com>
2017-10-10 20:23:39 +0100

    First Commit

---
+++ src/components/Main.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<h1>{{ title }}</h1>
+			<h3> {{ desc }} </h3>
+		</div>
+	</div>
+	<div class="row poll-box">
+		<form class="container">
+			<div class="form-group">
+				<p>Enter your question here: </p>
+				<textarea></textarea>
+			</div>
+
+			<div class="form-group">
+				<p>Add your options here: </p>
+				<div class="poll-options">
+					<input autocompelete="off" type="text" placeholder="Option 1">
+				</div>
+			</div>
+
+			<div class="">
+				<button type="submit" class="btn btn-default">Create Poll</button>
+			</div>
+		</form>
+	</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data() {
+    return {
+	  title: 'Poll App',
+	  desc: 'Create a poll for your organization'
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.main-container {
+	text-align: center;
+}
+h1 {
+	font-family: "Oswald", sans-serif;
+	font-weight: bold;
+ 
+}
+
+h3 {
+	margin-top: 50px;
+	font-family: "Oswald", sans-serif;
+	font-weight: 100;
+	color: #7f8c8d;
+}
+
+.poll-box {
+	margin-top: 20px;
+}
+form > div > p {
+	font-family: "Oswald", sans-serif;
+	font-size: 1.17em;
+}
+textarea {
+	height: 180px;
+	width: 50%;
+	border: none;
+	border-radius: 8px;
+	color: #4E4868;
+	font-size: 26px;
+	font-weight: 500;
+	line-height: 30px;
+	outline: none;
+	padding: 24px 16px;
+	resize: none;
+	text-align: center;
+}
+
+input[type=text] {
+	border: 0;
+	box-sizing: border-box;
+	width: 20%;
+	max-width: 20%;
+	border-radius: 7px;
+	border: none;
+	padding: 8px 16px;
+	outline: none;
+	text-align: center;
+	margin-bottom: 10px;
+	line-height: 36px;
+	font-size: 18px;
+
+}
+
+button {
+	box-sizing: border-box;
+	margin: 0;
+	width: 20%;
+	background-color: #4E4868;
+	border-radius: 8px;
+	color : #ffffff;
+	/* display: inline-block; */
+	font-size: 20px;
+	font-weight: 500;
+	padding: 24px;
+	text-align: center;
+	text-decoration: none;
+}
+</style>
